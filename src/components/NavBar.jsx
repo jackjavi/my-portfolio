c8ba4a112f145207dfa0264aea8d6eac0de5a712
jackjavi <jackmtembete@gmail.com>
2022-12-27 13:12:46 +0300

    Navbar small screen fonts fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -59,11 +59,11 @@ const NavBar = () => {
       </div>
 
       {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gray-200 text-gray-700">
           {links.map(({ id, link }) => (
             <li
               key={id}
-              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+              className="px-4 cursor-pointer capitalize py-6 text-2xl sm:text-3xl md:text-4xl shadow-md  w-full"
             >
               <Link
                 onClick={() => setNav(!nav)}
